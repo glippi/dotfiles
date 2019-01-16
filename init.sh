f2a616cfc65dbe2f56b014dae4a2297340b41b7a
Gabriele Lippi <gabriele@lippi.net>
2019-01-17 00:01:12 +0100

    Probably this will work

--- init.sh
+++ init.sh
@@ -8,20 +8,20 @@ rm -rf ~/.zshrc ~/.vim ~/.vimrc ~/.config/nvim/ ~/.tmux ~/.tmux.conf 2 > /dev/nu
 
 #create neovim directory
 echo "-= Creating necessary directories =-"
-mkdir -p ~/.config ~/.config/nvim
+mkdir -p ~/.config ~/.config/nvim ~/.config/nvim/nvim-partials
 
 echo "-= Symlinking new configs =-"
-ln -s ${wd}/zshrc ~/.zshrc
-ln -s ${wd}/tmux.conf ~/.tmux.conf
-ln -s ${wd}/init.vim ~/.config/nvim/init.vim
-"ln -s ${wd}/nvim-partials/settings.vim ~/.config/nvim/nvim-partials/settings.vim
-ln -s ${wd}/nvim-partials/plugins.vim ~/.config/nvim/nvim-partials/plugins.vim
-ln -s ${wd}/nvim-partials/mappings.vim ~/.config/nvim/nvim-partials/mappings.vim
-ln -s ${wd}/nvim-partials/abbreviations.vim ~/.config/nvim/nvim-partials/abbreviations.vim
-ln -s ${wd}/nvim-partials/statusline.vim ~/.config/nvim/nvim-partials/statusline.vim
-ln -s ${wd}/nvim-partials/ale.vim ~/.config/nvim/nvim-partials/ale.vim
-ln -s ${wd}/nvim-partials/fzf.vim ~/.config/nvim/nvim-partials/fzf.vim
-ln -s ${wd}/nvim-partials/functions.vim ~/.config/nvim/nvim-partials/functions.vim
-ln -s ${wd}/nvim-partials/colorscheme.vim ~/.config/nvim/nvim-partials/colorscheme.vim"
+ln -sT ${wd}/zshrc ~/.zshrc
+ln -sT ${wd}/tmux.conf ~/.tmux.conf
+ln -sT ${wd}/nvim-partials/settings.vim ~/.config/nvim/nvim-partials/settings.vim
+ln -sT ${wd}/nvim-partials/plugins.vim ~/.config/nvim/nvim-partials/plugins.vim
+ln -sT ${wd}/nvim-partials/mappings.vim ~/.config/nvim/nvim-partials/mappings.vim
+ln -sT ${wd}/nvim-partials/abbreviations.vim ~/.config/nvim/nvim-partials/abbreviations.vim
+ln -sT ${wd}/nvim-partials/statusline.vim ~/.config/nvim/nvim-partials/statusline.vim
+ln -sT ${wd}/nvim-partials/ale.vim ~/.config/nvim/nvim-partials/ale.vim
+ln -sT ${wd}/nvim-partials/fzf.vim ~/.config/nvim/nvim-partials/fzf.vim
+ln -sT ${wd}/nvim-partials/functions.vim ~/.config/nvim/nvim-partials/functions.vim
+ln -sT ${wd}/nvim-partials/colorscheme.vim ~/.config/nvim/nvim-partials/colorscheme.vim
+ln -sT ${wd}/init.vim ~/.config/nvim/init.vim
 
 echo "-= Log out and Log Back In to see changes =-"
