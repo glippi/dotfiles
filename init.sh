da733e781fbe026779dc5f7ffe8b671db398b486
glippi <gabriele@lippi.net>
2022-10-21 13:13:54 +0200

    Install clojure-lsp in /home/glippi

--- init.sh
+++ init.sh
@@ -3,5 +3,6 @@
 set -xe
 
 stow --target=$HOME stowed
+cd && sudo bash < <(curl -s https://raw.githubusercontent.com/clojure-lsp/clojure-lsp/master/install)
 chsh -s `which fish`
 ~/.config/nvim/sync.sh
